15dcdad41b7a1e0c103592d8aec21c2151a68f5c
VÕ VŨ HOÀNG <vvhoangvn@gmail.com>
2026-09-08 13:35:47 +0700

    Enable assessment conversion layer

--- assets/js/assessment.js
+++ assets/js/assessment.js
@@ -1,7 +1,9 @@
 import { initRealityCheck } from "./investment-reality-check.js";
 import { initInvestorProfileAssessment } from "./investor-profile-assessment.js";
+import { initAssessmentConversion } from "./assessment-conversion.js";
 
 export function initAssessment() {
   initRealityCheck();
   initInvestorProfileAssessment();
+  initAssessmentConversion();
 }
